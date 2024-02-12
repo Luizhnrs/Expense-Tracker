49ebc6b6520d5fddcb14d855f5a1a40a809472e8
Luizhnrs <Luizhnrsg@gmail.com>
2024-02-12 19:57:07 -0300

    Feat: Category Implemented

---
+++ Expense-Tracker/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Column]
+        public string Title { get; set; }
+        [Column]
+        public string Icon { get; set; }
+        [Column]
+        public string Type { get; set; }
+    }
+}
